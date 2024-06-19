1b6660b503463a5ff4ffd09047279d598a8dbb2e
rabeeh_rahman <rabeehrahman.rr@gmail.com>
2024-06-19 14:03:59 +0530

    connecy to db

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import { error } from 'console'
+
+dotenv.config()
+
+mongoose.connect(process.env.MONGO)
+.then(() => {
+    console.log('database connected')
+})
+.catch((err) => {
+    console.log(err)
+})
 
 const App = express()
 
